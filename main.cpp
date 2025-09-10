828c049f78cc089bce15badab05274aaf5f2b919
PranamyaRajeevan <pranamyar060420@gmail.com>
2025-09-10 21:17:44 +0000

    Add c++ program

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    string message = "Hello, World! I was built on Jenkins.";
+    cout << message;
+
+    return 0;
+}
+
